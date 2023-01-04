ddcaf20aa7e80bf284656ccbcb1022388a949686
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-04 11:18:19 +0530

    Add files via upload

---
+++ JavaScript Training/Day2/ques1.js
@@ -0,0 +1,7 @@
+        // 1. Declare all the variables in above form without initialising and print their value.
+        let Name;
+          console.log(Name);
+        let Address;
+          console.log(Address);
+        const b = 0;
+          console.log(b);
---
+++ JavaScript Training/Day2/ques2.js
@@ -0,0 +1,4 @@
+        // 2. After printing their value assign them as null and print it again.
+        //console.log(a);
+        let a = null;
+          console.log(a);
