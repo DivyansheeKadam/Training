a258ea87c000ad7c41b04b5b3844a0274dea8d33
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-05 22:10:07 +0530

    Update ques3.js

--- JavaScript Training/Day8/ques3.js
+++ JavaScript Training/Day8/ques3.js
@@ -1,23 +1,18 @@
 // // 3. Create a function that takes 2 parameter => an array of employees and an attribute and returns all employees sorted based on the empolees attribute being passed as second parameter.
-const printSort = (employees, order) => {
-  let fName;
-  let sName;
-  let sortingEmployee = employees.sort((x, y) => {
-    fName = x.name;
-    sName = y.name;
-    let comparison = 0;
-    if (fName > sName) {
-      comparison = 1;
-    } else if (fName < sName) {
-      comparison = -1;
+const printSort = (employees, attribute) => {
+   const innerSort = (objA, objB) => {
+    if (objA[attribute] < objB[attribute]) {
+      return -1 ;
     }
-    return ((order === 'asc') ? comparison : (comparison * -1));
-  });
-  console.log(sortingEmployee);
-};
-const employees=[{name: "Ram", email: "ram@gmail.com", phoneNo: 9876543211, salary: 20000, address: "Indore(M.P.)"},
-{name: "Sita",  email: "sita@gmail.com",  phoneNo: 8669841233, salary: 25000, address: "Bhopal(M.P.)"},
-{name: "Mohan", email: "mohan@gmail.com", phoneNo: 1587954133, salary: 40000, address: "Dhar(M.P.)"},
-{name: "Rohan", email: "rohan@gmail.com", phoneNo: 1236547896, salary: 30000, address: "Ujjain(M.P.)"},
-{name: "Shyam", email: "shyam@gmail.com", phoneNo: 4698514653, salary: 35000, address: "Indore(M.P.)"}];
-printSort(employees, "asc");
+      if (objA[attribute] > objB[attribute]) {
+      return 1;
+    }
+      return 0;
+  }
+  console.log(employees.sort(innerSort));
+  }
+const employees=[{name: "Ram", email: "ram@gmail.com", phoneNo: 9876543211, salary: 40000, address: "Indore(M.P.)"},
+{name: "Sita",  email: "sita@gmail.com",  phoneNo: 8669841233, salary: 30000, address: "Bhopal(M.P.)"},
+{name: "Rohan", email: "rohan@gmail.com", phoneNo: 1236547896, salary: 25000, address: "Ujjain(M.P.)"},
+{name: "Mohan", email: "mohan@gmail.com", phoneNo: 4698514653, salary: 35000, address: "Indore(M.P.)"}];
+printSort(employees, 'salary');
