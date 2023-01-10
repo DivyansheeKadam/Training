bcabb5986c0612e69920749a40bb08a8b7dfa258
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-10 15:56:45 +0530

    Update ques2.js

--- JavaScript Training/Day10/ques2.js
+++ JavaScript Training/Day10/ques2.js
@@ -1,12 +1,20 @@
 //- Use the class in index file to create few employees and use the functions in class on them"
 class Index {
-    constructor (name,age,city,married) {
-        console.log(name,age,city,married);
+    constructor (name, age, city, married) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.married = married;
+        console.log(this.name, this.age, this.city, this.married);
     };
-    obj (name,age,city,married) {
-        console.log(name,age,city,married);
-    };
-}
+    obj (name, age, city, married) {
+        let empNew= {"name" : name,
+        "age" : age,
+        "city" : city,
+        "married" : married}
+        console.log(empNew); 
+       };
+};
 let a = new Index("Radhu", 25, "Indore", false);
 a.obj("Ram", 25, "Indore", true);
 a.obj("sita", 22, "Dhar", true);
