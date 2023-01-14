075d3648115b465580461a39e254f72977ee8871
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-14 21:03:57 +0530

    Update ques1.js

--- JavaScript Training/Day4/ques1.js
+++ JavaScript Training/Day4/ques1.js
@@ -1,9 +1,13 @@
 // 1. Take two numbers and perform all the arithmetic operations over them. 
         const a = 20;
         const b = 10;
-        const sum = a + b;
-        const sub = a - b;
-        const mul = a * b;
-        const div = a / b;
-            console.log("Sum" + sum + " Sub" + sub);
-            console.log("Multipul " + mul + " Divi" + div);
+        const addition = a + b;
+            console.log(`Addition ${addition}`);
+        const subtraction = a - b;
+            console.log(`Subtraction ${subtraction}`);
+        const multiply = a * b;
+            console.log(`Multiply ${multiply}`);
+        const divide = a / b;
+            console.log(`Divide ${divide}`);
+        const modules = a % b;
+            console.log(`Modules ${modules}`);
