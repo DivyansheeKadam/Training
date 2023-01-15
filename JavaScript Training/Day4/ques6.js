7f42f5d26f24cb7ba501ae2cb7c27a2c3e326a81
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-15 21:41:44 +0530

    Update ques6.js

--- JavaScript Training/Day4/ques6.js
+++ JavaScript Training/Day4/ques6.js
@@ -1,19 +1,11 @@
 // 6. Take 2 variables and compare them using == and === such that result of both the comparisons operation 
 // is not equal.
     // //compare ==
-   const z = 10;
-   if (z % 2 == 0) {
-    console.log("its even no.");
-    } else {
-        console.log("its odd no.");
-    }
+   const x = 20;
+   const y = 10;
+   const a = (x == y);
+    console.log(a);
+
     // //compare ===
-    const c = "2";
-    let is = false;
-    if (c === 2) {
-        is = true;
-        console.log(is);
-        } else {
-        is = false;
-        console.log(is);
-    };
+    const b = (x === y);
+      console.log(b);
