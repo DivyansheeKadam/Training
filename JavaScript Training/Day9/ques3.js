2cda37a7d1ed8b28583e74696311a2eea063bd01
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-17 22:37:45 +0530

    Update ques3.js

--- JavaScript Training/Day9/ques3.js
+++ JavaScript Training/Day9/ques3.js
@@ -1,15 +1,14 @@
+// 3. Take another paramter as salary and keep it above 500000 in numbers in json and print it upto one decimal place only with unit as Lac"
 import dataforM from "./ques2.js";
 const salary=()=>{
     let print="";
     const salaryfind = dataforM.filter((data) => {
         if (data.salary >500000){
-            return print += ((data.salary/100000).toFixed(1))
+            return print += ((data.salary/100000).toFixed(1)) + ", "
         }
     });
-
     console.log(salaryfind)
-    console.log(print)
-
+    console.log("One decimal " + print )
 }
 salary()
 export default salary;
