0b025542927d0f0142655fb1f2aaa5e98dc6443a
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-18 09:44:14 +0530

    Update ques1.js

--- JavaScript Training/Day_13/ques1.js
+++ JavaScript Training/Day_13/ques1.js
@@ -13,31 +13,44 @@ const example = (employees) => {
     let empSecond = new Promise((resolve, reject)=>{
         setTimeout(()=>{
            if (employees[1]) {
-            resolve(employees[1])
+            resolve(employees[1]);
         } else {
-            reject("its not have employee data")
+            reject("its not have employee data");
         }
 },2000)
     });
     let empThird = new Promise((resolve, reject)=>{
         setTimeout(()=>{
-           if (employees[i]) {
-            resolve(employees[2]) 
+           if (employees[2]) {
+            resolve(employees[2]);
         } else {
-            reject("its not have employee data")
+            reject("its not have employee data");
         }
         
 },3000)
+    });
+    let empFourth = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+           if (employees[3]) {
+            resolve(employees[3]);
+        } else {
+            reject("its not have employee data");
+        }
+        
+},4000)
     });
      empFirst
             .then(result => {console.log(result)
             return empSecond})
             .then(result => {console.log(result)
                 return empThird})
-            .then(result => console.log(result))
+            .then(result => {console.log(result)
+                return empFourth})
+                .then(result => console.log(result))
      .catch(err=>console.log(err))
-    }
-}
+    break;
+    };
+};
 const employees = [{name: "Ram", email: "ram@gmail.com", phoneNo: 9876543211, salary: 40000, address: "Indore(M.P.)"},
 {name: "Sita",  email: "sita@gmail.com",  phoneNo: 8669841233, salary: 30000, address: "Bhopal(M.P.)"},
 {name: "Rohan", email: "rohan@gmail.com", phoneNo: 1236547896, salary: 25000, address: "Ujjain(M.P.)"},
