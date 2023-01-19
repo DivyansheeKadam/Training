48452087918abaff8746c982b5352116286c4f0c
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-19 16:13:40 +0530

    Update ques2.js

--- JavaScript Training/Day8/ques2.js
+++ JavaScript Training/Day8/ques2.js
@@ -1,6 +1,6 @@
 // 2. Create a function that takes 2 parameter => an array of employees and age and returns all employees whose age is less than input age.
-const printAge = (emp, n) => {
-    const employeeAge = emp.filter((employee) => employee.age < n)
+const printAge = (emp, age) => {
+    const employeeAge = emp.filter((employee) => employee.age < age)
       console.log(employeeAge);
 }
 const employee=[{name: "Ram", age: 25},
@@ -8,4 +8,5 @@ const employee=[{name: "Ram", age: 25},
 {name: "Mohan", age: 52},
 {name: "Rohan", age: 55},
 {name: "Shyam", age: 40}];
+
 printAge(employee, 50);
