2fcb4f7aa92285484cd7c97e23518a4c2ddbaba6
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-19 15:12:26 +0530

    Update quest1.js

--- JavaScript Training/Day3/quest1.js
+++ JavaScript Training/Day3/quest1.js
@@ -1,11 +1,9 @@
 //1. In the variables declare above assign the variables as appropriate default value instead of null.
-const Bigint = BigInt("13165646987885456468644555");
-        console.log(Bigint);
-        
-const x = 6.888895593077233e+308;
-const Infi = x * 66546556.465602315644;
-        console.log(Infi);
-
-const x1 = (-6.888895593077233e+308);
-const MInfinity = x1 * 66546556.465602315644;
-        console.log(MInfinity);
+var  a;
+        a = 10;
+        console.log(a);
+let b;
+        b = 20;
+        console.log(b);
+const  c = 10;
+        console.log(c);
