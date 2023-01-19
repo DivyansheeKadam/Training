f53ed6598e7c3b2fc4cddde380216059369d2e56
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-19 18:01:53 +0530

    Update quest1.js

--- JavaScript Training/Day3/quest1.js
+++ JavaScript Training/Day3/quest1.js
@@ -1,9 +1,11 @@
 //1. In the variables declare above assign the variables as appropriate default value instead of null.
-var  a;
         a = 10;
+        var  a;
         console.log(a);
-let b;
+
+        let b;
         b = 20;
         console.log(b);
-const  c = 10;
+
+        const  c = 10;
         console.log(c);
