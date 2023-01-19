9c672b7e45f53d83523247b03c669ba4d038df49
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-19 11:30:22 +0530

    Update ques6.js

--- JavaScript Training/Day4/ques6.js
+++ JavaScript Training/Day4/ques6.js
@@ -1,12 +1,11 @@
 // 6. Take 2 variables and compare them using == and === such that result of both the comparisons operation is not equal.
-    // //compare ==
+    //compare ==
    const x = 20;
-   const y = 10;
+   const y = "20";
    const a = (x == y);
       console.log(a);
 
     // //compare ===
     const b = (x === y);
         console.log(b);
-
     console.log(a !== b);
