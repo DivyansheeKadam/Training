2ff9019be278d667a07083cc4435cb532fcf2bd4
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-19 15:42:28 +0530

    Update ques4.js

--- JavaScript Training/Day4/ques4.js
+++ JavaScript Training/Day4/ques4.js
@@ -1,4 +1,42 @@
 // 4. Write an operation to get its result as NaN. 
-    let x = 0;
+//number with undefined 
+const x = 2;
     let y;
     console.log(x + y);
+    console.log(x / y);
+    console.log(x * y);
+    console.log(x - y);
+    console.log(x % y);
+
+//string with null
+    const a = "Hellow";
+    const b = null;
+        console.log(a - b);
+        console.log(a * b);
+        console.log(a / b);
+        console.log(a % b);
+
+//Number with NaN
+    const c = 20;
+    const d = NaN;
+        console.log(c * d);
+        console.log(c - d);
+        console.log(c / d);
+        console.log(c + d);
+        console.log(c % d);
+
+//string with any numbers
+    const e = "hellow";
+        const f = 8;
+            console.log(e - f);
+            console.log(e * f);
+            console.log(e / f);
+            console.log(e % f);
+
+//string with NaN
+const g = "hellow";
+const h = NaN;
+    console.log(g - h);
+    console.log(g * h);
+    console.log(g / h);
+    console.log(g % h);
