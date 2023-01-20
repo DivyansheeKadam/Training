72f866e1994d54aec3e5c2fb04359b1c02303e87
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-20 09:29:41 +0530

    Update ques2and3.js

--- JavaScript Training/Day4/ques2and3.js
+++ JavaScript Training/Day4/ques2and3.js
@@ -3,9 +3,9 @@ const num1 = 10;
 const num2 = 10;
 const string1 = "Sita";
 const string2 = "Ram";
-    console.log(`Numbers:- ${num1 + string1}`)
-    console.log(`Strings:- ${ num2 + string2}`)
+    console.log(`Number with string:- ${num1 + string1}`)
+    console.log(`Number with string:- ${ num2 + string2}`)
 
 // 3. Take 2 Strings and 2 numbers and perform addition operation on them in same sequence. 
-    console.log(`Strings:- ${string1 + num1}`);
-    console.log(`Numbers:- ${string2 + num2}`);
+    console.log(`String with number:- ${string1 + num1}`);
+    console.log(`String with number:- ${string2 + num2}`);
