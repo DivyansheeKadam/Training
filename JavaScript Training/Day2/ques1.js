70e4a42b2f60caf14f39990a3cc81be5202373e4
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-20 09:03:42 +0530

    Update ques1.js

--- JavaScript Training/Day2/ques1.js
+++ JavaScript Training/Day2/ques1.js
@@ -1,7 +1,15 @@
-        // 1. Declare all the variables in above form without initialising and print their value.
-        var Name;
-          console.log(Name);
-        let Address;
-          console.log(Address);
-        const b = 0;
-          console.log(b);
+ // 1. Declare all the variables in above form without initialising and print their value.
+    let Name;
+        console.log(Name);
+    let Email;
+        console.log(Email);
+    let Age;
+        console.log(Age);
+    let address;
+        console.log(address);
+    let Phone;
+        console.log(Phone);
+    let Gender;
+        console.log(Gender);
+    let married_status;
+        console.log(married_status);
