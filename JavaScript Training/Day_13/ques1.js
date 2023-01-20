5905b78e5b3327061bfee67b3776c773f4cd3e40
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-20 11:58:08 +0530

    Update ques1.js

--- JavaScript Training/Day_13/ques1.js
+++ JavaScript Training/Day_13/ques1.js
@@ -1,33 +1,19 @@
-// " - Create a function that returns array of employees from a random time from 1 to 2 seconds using promise chaining.
-const example = (employees) => {
-    new Promise(function(resolve, reject) {
-        let time = (Math.random()*2)+1000
+// // " - Create a function that returns array of employees from a random time from 1 to 2 seconds using promise chaining.
+const empData = (employees) => {
+      return new Promise((resolve, reject) => {
         setTimeout(() => {
-        if(employees){
-            resolve(employees)
+        if (employees) {
+          resolve(employees);
         } else {
-            reject("Data Error")
-        }}, time);
-      }).then((result) => {
-            return result[0];
-      }).then((result) => {
-            console.log(result);  
-            return employees[1];
-      }).then((result) => { 
-            console.log(result); 
-            return employees[2];
-      }).then(function(result) {
-            console.log(result);
-            return employees[3];
-      }).then((result => console.log(result)))
-      .catch((err)=>{
-            console.log(err);
+          reject("Data Error");
+        }
+        }, (Math.random * 2) + 1000)
       });
-    
-};
-const employees = [{name: "Ram", email: "ram@gmail.com", phoneNo: 9876543211, salary: 40000, address: "Indore(M.P.)"},
-{name: "Sita",  email: "sita@gmail.com",  phoneNo: 8669841233, salary: 30000, address: "Bhopal(M.P.)"},
-{name: "Rohan", email: "rohan@gmail.com", phoneNo: 1236547896, salary: 25000, address: "Ujjain(M.P.)"},
-{name: "Mohan", email: "mohan@gmail.com", phoneNo: 4698514653, salary: 35000, address: "Indore(M.P.)"}];
-
-example(employees);
+      };
+      const employees = [{name: "Ram", email: "ram@gmail.com", phoneNo: 9876543211, salary: 40000, address: "Indore(M.P.)"},
+      {name: "Sita",  email: "sita@gmail.com",  phoneNo: 8669841233, salary: 30000, address: "Bhopal(M.P.)"},
+      {name: "Rohan", email: "rohan@gmail.com", phoneNo: 1236547896, salary: 25000, address: "Ujjain(M.P.)"},
+      {name: "Mohan", email: "mohan@gmail.com", phoneNo: 4698514653, salary: 35000, address: "Indore(M.P.)"}];
+      
+      empData(employees).then(result => console.log(result))
+                        .catch(err =>console.log(err));
