a73e10333f16a692dbda88322b6223ee34ea956b
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-20 09:15:06 +0530

    Update ques3.js

--- JavaScript Training/Day2/ques3.js
+++ JavaScript Training/Day2/ques3.js
@@ -1,7 +1,9 @@
 // 3. Declare an object called employeeDetails and add all the above variables to this object now as keys with value as null and print this object."
-const employeeDetails = {Name: "Ram", 
-Email: "ram@gmail.com", 
-PhoneNo: 9876543211, 
-Address: "Indore(M.P.)", 
-age: null };
+const employeeDetails = {Name: null, 
+Email: null, 
+PhoneNo: null, 
+Address: null, 
+age: null,
+gender: null,
+married_status: null };
    console.log(employeeDetails);
