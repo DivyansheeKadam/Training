d1338004a96557fa6e3fee8d0f494b96a7a03fba
DivyansheeKadam <103111723+DivyansheeKadam@users.noreply.github.com>
2023-01-24 14:04:04 +0530

    Update ques2.js

--- JavaScript Training/Day2/ques2.js
+++ JavaScript Training/Day2/ques2.js
@@ -1,4 +1,4 @@
         // 2. After printing their value assign them as null and print it again.
-        //console.log(a);
-        let a = null;
+        console.log(a);
+        var a = null;
           console.log(a);
